a1357916b8ce41cdd88cb8be303fb49a00d5ae12
David <dguillen12344321@gmail.com>
2023-10-30 19:14:52 -0300

    changes: - Changed database url

--- apiDevDavidG/settings.py
+++ apiDevDavidG/settings.py
@@ -93,7 +93,7 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
