18ef96e0af51f7d2de904dcf252ec347bed01fca
David <dguillen12344321@gmail.com>
2023-10-30 16:42:52 -0300

    .

--- apiDevDavidG/urls.py
+++ apiDevDavidG/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from myapp.views import ProjectCardList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('projectcards/', ProjectCardList.as_view(), name='projectcard-list'),
+    path('projectcards/', ProjectCardList.as_view(), name='projectcard-api'),
 ]
